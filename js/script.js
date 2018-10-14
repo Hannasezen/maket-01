18c50986af9883569e2716866a2f1dcf9704d7bd
Sezen Anna <41483582+Hannasezen@users.noreply.github.com>
2018-10-14 19:41:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,5 +12,4 @@ function openPainment(evt, Painment) {
 		    evt.currentTarget.className += " active";
 }
 
-		// Get the element with id="defaultOpen" and click on it
 		document.getElementById("defaultOpen").click();
